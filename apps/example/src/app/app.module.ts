b205e903a618a00260609aa2251f4f8df54bc51d
Ahmed Raza <hafizahmedmoon@gmail.com>
2020-06-28 03:37:09 +0500

    add ionic

--- apps/example/src/app/app.module.ts
+++ apps/example/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, IonicModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
